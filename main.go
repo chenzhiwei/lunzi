4ac5b0b37bf2552db1ad69e586cb329e657e7afd
Chen Zhiwei <zhiweik@gmail.com>
2022-05-28 08:23:24 +0800

    use golang native module

--- main.go
+++ main.go
@@ -2,47 +2,46 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"net/http"
 	"os/exec"
 	"strings"
-
-	"github.com/gin-gonic/gin"
 )
 
-type Form struct {
-	Text string `form:"cmd"`
-}
-
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
-
-	v1 := r.Group("/v1")
-	{
-		v1.POST("/exec", handler)
-	}
-
-	r.Run("0.0.0.0:8080")
+	http.HandleFunc("/", help)
+	http.HandleFunc("/v1/exec", v1Exec)
+	fmt.Println("listen on :8080")
+	http.ListenAndServe(":8080", nil)
 }
 
-func handler(c *gin.Context) {
-	var form Form
-	c.ShouldBind(&form)
+func help(w http.ResponseWriter, r *http.Request) {
+	helpString := `
+curl http://localhost:8080/
+curl http://localhost:8080/ping
+curl -X POST http://localhost:8080/v1/exec --form-string cmd="ls -al /tmp"
+curl -X POST http://localhost:8080/v1/exec --form-string cmd="if true; then echo ok; fi"
+curl -X POST http://localhost:8080/v1/exec --form-string cmd="whatever cmd you want to run"
+
+`
+	fmt.Fprintf(w, helpString)
+}
+
+func v1Exec(w http.ResponseWriter, r *http.Request) {
+	text := r.FormValue("cmd")
 
 	var cmd *exec.Cmd
 	_, err := exec.LookPath("bash")
 	if err == nil {
-		cmd = exec.Command("bash", "-c", form.Text)
+		cmd = exec.Command("bash", "-c", text)
 	} else {
-		cmd = exec.Command("sh", "-c", form.Text)
+		cmd = exec.Command("sh", "-c", text)
 	}
 
 	stdout, _ := cmd.StdoutPipe()
 	stderr, _ := cmd.StderrPipe()
 	if err := cmd.Start(); err != nil {
-		c.String(http.StatusBadRequest, err.Error())
+		fmt.Fprintf(w, err.Error())
 		return
 	}
 
@@ -57,5 +56,5 @@ func handler(c *gin.Context) {
 		result = append(result, errScanner.Text())
 	}
 
-	c.String(http.StatusOK, strings.Join(result, "\n"))
+	fmt.Fprintf(w, strings.Join(result, "\n"))
 }
